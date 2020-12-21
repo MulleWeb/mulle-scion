442773029f9dd11fdc5547e5187a940f3e7bb603
Nat! <nat@codeon.de>
2020-12-21 14:25:27 +0100

    version and RELEASENOTES

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,4 +1,7 @@
+### 1859.1.5
+
+* Various small improvements
+
 ### 1859.1.4
 
 * split off from original MulleScion
-
--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 /*
  *  version:  major, minor, patch
  */
-#define MULLE_SCION_VERSION  ((1859 << 20) | (1 << 8) | 4)
+#define MULLE_SCION_VERSION  ((1859 << 20) | (1 << 8) | 5)
 
 
 static inline unsigned int   mulle_scion_get_version_major( void)
