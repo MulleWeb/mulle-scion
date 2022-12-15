8c79bc2d02fcf8bc45f0e1095841a361485f7297
Nat! <nat@mulle-kybernetik.com>
2022-12-15 16:35:56 +0100

    get rid of old trash file

--- test/.mulle/etc/env/environment-post-global.sh
+++
@@ -1,7 +0,0 @@
-# Edit with: mulle-env environment --scope post-global set <key> <value>
-#
-#
-#
-export PATH="${DEPENDENCY_DIR}/Debug/bin:${DEPENDENCY_DIR}/Release/bin:${DEPENDENCY_DIR}/bin:$PATH"
-
-
